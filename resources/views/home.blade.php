6baaa76a6dbb84afdd4c4f41d27c78855e08eaf4
accoustic20 <39542235+accoustic20@users.noreply.github.com>
2018-05-23 11:55:54 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,42 +10,40 @@
           <h2 class="page-header">Home Agent</h2>
         @endif
       </div>
-      <!-- /.col-lg-12 -->
+
   </div>
 
   <div class="row">
     <div class="col-lg-8">
-
             <div class="panel-body" id="calls">
               <canvas id="reportQuestion" height="500" width="700"></canvas>
             </div>
-
     </div>
+    <!-- Report Question -->
+
 
   <div class="row">
     <div class="col-lg-3">
-            <!-- /.panel-heading -->
             <div class="panel-body" id="calls">
               <canvas id="reportCalls" height="500" width="400"></canvas>
             </div>
-            <!-- /.panel-body -->
-
-        <!-- /.panel -->
     </div>
+    <!-- Report Ticket -->
 
 
     <div class="col-lg-6">
-            <!-- /.panel-heading -->
             <div class="panel-body" id="calls">
               <canvas id="reportKanwil" height="500" width="600"></canvas>
             </div>
-        </div><!-- /.panel -->
+    </div>
+    <!-- Report Kanwil -->
 
     <div class="col-lg-6">
-            <!-- /.panel-heading -->
             <div class="panel-body" id="calls">
               <canvas id="reportCabang" height="500" width="600"></canvas>
             </div>
-        </div><!-- /.panel -->
+    </div>
+    <!-- Report Cabang -->
+  </div>
 
 @endsection
