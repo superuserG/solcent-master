e73cc6826b65cf3aadba6525a000b90ee71e174e
accoustic20 <39542235+accoustic20@users.noreply.github.com>
2018-06-07 09:44:25 +0700

    january
    asd

--- app/Http/Controllers/monthController.php
+++ app/Http/Controllers/monthController.php
@@ -3,11 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+
 
 class monthController extends Controller
 {
   public function showJanuary(){
-    return view('Page_Month.january');
+
+    //call
+    $present = DB::table('report_calls')
+          ->select('presentedCall as presented')
+          ->where('months','=','Januari')
+          ->get()->toArray();
+    $present = array_column($present,'presented');
+
+    $handle = DB::table('report_calls')
+          ->select('handledCall as handled')
+          ->where('months','=','Januari')
+          ->get()->toArray();
+    $handle = array_column($handle,'handled');
+
+    $abandon = DB::table('report_calls')
+          ->select('abandonedCall as abandoned')
+          ->where('months','=','Januari')
+          ->get()->toArray();
+    $abandon = array_column($abandon,'abandoned');
+
+    // compare
+    $calljan = DB::table('report_calls')
+          ->select('presentedCall as call')
+          ->where('months','=','Januari')
+          ->get()->toArray();
+    $calljan = array_column($calljan,'call');
+
+    $ticket_jan = DB::table('report_solcents')
+          ->select(DB::raw('Count(WO_ID) as ticket'))
+          ->whereMonth('Submit_Date','=',date('01'))
+          ->get()->toArray();
+    $ticket_jan = array_column($ticket_jan,'ticket');
+
+    // top question
+    $question = DB::table('report_solcents')
+              ->select(array(DB::raw('count(Category_1) as Question')))
+              ->whereMonth('Submit_Date','=',date('01'))
+              ->groupBy('Category_1')
+              ->orderBy('Question','desc')
+              ->get()->toArray();
+    $question = array_column($question, 'Question');
+
+    $cat = DB::table('report_solcents')
+            ->select(array('Category_1 as Cat', DB::raw('count(Category_1) as result')))
+            ->whereMonth('Submit_Date','=',date('01'))
+            ->take(10)
+            ->groupBy('Category_1')
+            ->orderBy('Result','desc')
+            ->get()->toArray();
+    $cat = array_column($cat, 'Cat');
+
+
+    return view('Page_Month.january')
+    ->with('present',json_encode($present,JSON_NUMERIC_CHECK))
+    ->with('handle',json_encode($handle,JSON_NUMERIC_CHECK))
+    ->with('abandon',json_encode($abandon,JSON_NUMERIC_CHECK))
+    ->with('calljan',json_encode($calljan,JSON_NUMERIC_CHECK))
+    ->with('ticket_jan',json_encode($ticket_jan,JSON_NUMERIC_CHECK))
+    ->with('question',json_encode($question,JSON_NUMERIC_CHECK))
+    ->with('cat',json_encode($cat,JSON_NUMERIC_CHECK))
+    ;
   }
 
   public function showFebruary(){
--- resources/views/Chart/januaryChart.blade.php
+++ resources/views/Chart/januaryChart.blade.php
@@ -1,13 +1,17 @@
 <script>
   var label_A = ['Present','Handle','abandon'];
-  var data_call = [200,150,50];
+  var data_prensent = <?php echo $present;?>;
+  var data_handle = <?php echo $handle;?>;
+  var data_abandon = <?php echo $abandon;?>;
 
   var a = {
       labels: label_A,
       datasets:
       [{
           label: 'Call',
-          data: data_call,
+          data: [data_prensent,
+                data_handle,
+                data_abandon],
           backgroundColor:
           [
               'rgba(0,128,0, 0.5)'
@@ -15,10 +19,12 @@
       }],
 
   };
+  // Call
 
   var label_B = ['Present'];
-  var data_call_present = [200];
-  var data_ticket = [500];
+  var data_call_present = <?php echo $calljan;?>;
+  var data_ticket = <?php echo $ticket_jan;?>;
+
 
   var b = {
       labels: label_B,
@@ -47,6 +53,41 @@
     ],
 
   };
+  // Compare call and ticket
+
+  var label_C = <?php echo $cat;?>;
+  var data_top = <?php echo $question;?>;
+
+  var c = {
+    labels: label_C,
+    datasets:
+    [{
+        label: 'Top Category',
+        data: data_top,
+        backgroundColor:
+        [
+            'rgba(0,128,0, 0.5)'
+        ],
+    }],
+  };
+  // Top question
+
+  var label_D = ['Present'];
+  var data_topic = [200];
+
+  var d = {
+    labels: label_D,
+    datasets:
+    [{
+        label: 'Top Topic',
+        data: data_top,
+        backgroundColor:
+        [
+            'rgba(0,128,0, 0.5)'
+        ],
+    }],
+  };
+  //Top Topic
 
   window.onload = function(){
     var ja = document.getElementById("callJanuary").getContext("2d");
@@ -105,6 +146,66 @@
             title: {
                 display: true,
                 text: 'Calls and Tickets'
+            }
+        }
+    });
+
+    var jc = document.getElementById("topQuestionJan").getContext("2d");
+    var compareC = new Chart(jc, {
+        type: 'bar',
+        data: c,
+        options: {
+          scales: {
+             yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+             }],
+             xAxes: [{
+                     ticks: {
+                      fontSize: 10
+                     }
+                    }]
+            },
+            elements: {
+                rectangle: {
+                    borderSkipped: 'bottom'
+                }
+            },
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Top Question'
+            }
+        }
+    });
+
+    var jd = document.getElementById("topicJan").getContext("2d");
+    var compareD = new Chart(jd, {
+        type: 'bar',
+        data: d,
+        options: {
+          scales: {
+             yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+             }],
+             xAxes: [{
+                     ticks: {
+                      fontSize: 10
+                     }
+                    }]
+            },
+            elements: {
+                rectangle: {
+                    borderSkipped: 'bottom'
+                }
+            },
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Top Topic'
             }
         }
     });
