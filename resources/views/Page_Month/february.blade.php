33030b4a39744411a4d1ecf3f3edf4fe20c862e2
accoustic20 <39542235+accoustic20@users.noreply.github.com>
2018-07-06 09:14:36 +0700

    feature month and kanwil view
    view

--- resources/views/Page_Month/february.blade.php
+++ resources/views/Page_Month/february.blade.php
@@ -4,29 +4,26 @@
   @include('Chart.februaryChart')
 
   <div class="row">
-    <div class="col-lg-12">
-        <h2 class="page-header">February Page
-          <span class="dropdown col-lg-offset-8">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Choose Month
-                </button>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-              <li><a class="dropdown-item" href="{!! route('monthlyReport') !!}">All</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.January') !!}">January</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.February') !!}">February</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.March') !!}">March</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.April') !!}">April</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.May') !!}">May</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.June') !!}">June</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.July') !!}">July</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.August') !!}">August</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.September') !!}">September</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.October') !!}">October</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.November') !!}">November</a></li>
-              <li><a class="dropdown-item" href="{!! route('month.December') !!}">December</a></li>
-            </div>
-          </span>
+    <div class="row">
+      <div class="col-lg-6">
+          <h2 class="page-header">
+              February Page
+          </h2>
+      </div>
+
+      <div class="col-lg-6">
+        <h2 class="page-header">
+          <div class="class col-sm-3">
+            @include('Feature.feature_month')
+          </div>
+
+          <div class="class col-sm-3">
+            @include('Feature.feature_kanwil')
+          </div>
+          <br>
         </h2>
+      </div>
+
     </div>
 
   </div>
