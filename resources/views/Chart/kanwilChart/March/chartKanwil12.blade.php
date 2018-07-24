b590ca7b1fc1ee9e4d93a4d991984402ee15abf1
accoustic20 <39542235+accoustic20@users.noreply.github.com>
2018-07-24 10:31:43 +0700

    Show Value on chart

--- resources/views/Chart/kanwilChart/March/chartKanwil12.blade.php
+++ resources/views/Chart/kanwilChart/March/chartKanwil12.blade.php
@@ -49,9 +49,11 @@ var dataKcp = {
   }],
 };
 
+
 window.onload = function(){
-  var me = document.getElementById("kcu12").getContext("2d");
-  var compareE = new Chart(me, {
+
+  var me = document.getElementById("kcu12"),
+     compareE = new Chart(me, {
       type: 'bar',
       data: dataKcu,
       options: {
@@ -67,16 +69,35 @@ window.onload = function(){
                    }
                   }]
           },
-          elements: {
-              rectangle: {
-                  borderSkipped: 'bottom'
-              }
-          },
-          responsive: true,
-          title: {
+        elements: {
+            rectangle: {
+                borderSkipped: 'bottom'
+            }
+        },
+        responsive: true,
+        title: {
               display: true,
-              text: 'Top Ku'
-          }
+              text: 'Top KCU'
+          },
+        animation: {
+            duration: 1,
+            onComplete: function () {
+                var chartInstance = this.chart,
+                    ctx = chartInstance.ctx;
+                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'bottom';
+
+                this.data.datasets.forEach(function (dataset, i) {
+                    var meta = chartInstance.controller.getDatasetMeta(i);
+                    meta.data.forEach(function (bar, index) {
+                        var data = dataset.data[index];
+                        ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                    });
+                });
+            }
+        }
+
       }
   });
 
@@ -105,8 +126,27 @@ window.onload = function(){
           responsive: true,
           title: {
               display: true,
-              text: 'Top kp'
-          }
+              text: 'Top KCP'
+          },
+        animation: {
+            duration: 3,
+            onComplete: function () {
+                var chartInstance = this.chart,
+                    ctx = chartInstance.ctx;
+                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'bottom';
+
+                this.data.datasets.forEach(function (dataset, i) {
+                    var meta = chartInstance.controller.getDatasetMeta(i);
+                    meta.data.forEach(function (bar, index) {
+                        var data = dataset.data[index];
+                        ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                    });
+                });
+            }
+        }
+
       }
   });
 }
