13029726d3466028c04b524b2571dcf5230fb2ae
accoustic20 <39542235+accoustic20@users.noreply.github.com>
2018-07-24 10:45:03 +0700

    March Kanwil

--- resources/views/Chart/kanwilChart/March/chartKanwil9.blade.php
+++ resources/views/Chart/kanwilChart/March/chartKanwil9.blade.php
@@ -75,7 +75,25 @@ window.onload = function(){
           responsive: true,
           title: {
               display: true,
-              text: 'Top Ku'
+              text: 'Top KCU'
+          },
+          animation: {
+              duration: 3,
+              onComplete: function () {
+                  var chartInstance = this.chart,
+                      ctx = chartInstance.ctx;
+                  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'bottom';
+
+                  this.data.datasets.forEach(function (dataset, i) {
+                      var meta = chartInstance.controller.getDatasetMeta(i);
+                      meta.data.forEach(function (bar, index) {
+                          var data = dataset.data[index];
+                          ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                      });
+                  });
+              }
           }
       }
   });
@@ -105,7 +123,25 @@ window.onload = function(){
           responsive: true,
           title: {
               display: true,
-              text: 'Top kp'
+              text: 'Top KCU'
+          },
+          animation: {
+              duration: 3,
+              onComplete: function () {
+                  var chartInstance = this.chart,
+                      ctx = chartInstance.ctx;
+                  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'bottom';
+
+                  this.data.datasets.forEach(function (dataset, i) {
+                      var meta = chartInstance.controller.getDatasetMeta(i);
+                      meta.data.forEach(function (bar, index) {
+                          var data = dataset.data[index];
+                          ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                      });
+                  });
+              }
           }
       }
   });
--- resources/views/Chart/kanwilChart/March/chartKp.blade.php
+++ resources/views/Chart/kanwilChart/March/chartKp.blade.php
@@ -75,7 +75,25 @@ window.onload = function(){
           responsive: true,
           title: {
               display: true,
-              text: 'Top Ku'
+              text: 'Top Kantor Pusat'
+          },
+          animation: {
+              duration: 3,
+              onComplete: function () {
+                  var chartInstance = this.chart,
+                      ctx = chartInstance.ctx;
+                  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'bottom';
+
+                  this.data.datasets.forEach(function (dataset, i) {
+                      var meta = chartInstance.controller.getDatasetMeta(i);
+                      meta.data.forEach(function (bar, index) {
+                          var data = dataset.data[index];
+                          ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                      });
+                  });
+              }
           }
       }
   });
