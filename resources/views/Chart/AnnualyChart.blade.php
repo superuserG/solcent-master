0bfe93c14f6a5c1ac6e51e3dad468d9a1ac5863b
accoustic20 <39542235+accoustic20@users.noreply.github.com>
2019-01-25 14:21:22 +0700

    annual

--- resources/views/Chart/AnnualyChart.blade.php
+++ resources/views/Chart/AnnualyChart.blade.php
@@ -1,11 +1,76 @@
 <script>
-  var year = ['2018','2019','2020'];
-  var call_data = [123,456,789];
-  var callChart =
-  {
+  var months = ['January','February','Maret','April',
+              'Mei', 'Juni', 'July', 'Agustus',
+              'September','Oktober','November','Desember'];
+  var ticket_2018 = [14357,11842,15227,14552,
+                  15123,8661,15058,13424,
+                  11963,15065,14530,13006,
+                ];
+  var ticket_2017 = [10769,10841,13444,11358,
+                  13094,9262,13101,13390,
+                  11250,12754,12900,11372,
+                ];
+  var detailChart = {
+    labels: months,
+    datasets:
+    [{
+      label: 'Tickets 2018',
+      data: ticket_2018,
+      borderColor:
+      [
+        'rgba(0,128,0, 0.5)',
+      ],
+      backgroundColor:
+      [
+        'rgba(0,0,128, 0.0)',
+      ]
+    },{
+      label: 'Tickets 2017',
+      data: ticket_2017,
+      borderColor:
+      [
+        'rgba(0,0,255, 0.5)',
+      ],
+      backgroundColor:
+      [
+        'rgba(0,0,128, 0.0)',
+      ]
+    }
+  ]};
+
+  var year = ['2017','2018'];
+  var ticket_data = [143535,162808];
+  var handled_data = [ 155011, 173426];
+  var ticketChart = {
     labels: year,
     datasets:
     [{
+      label: 'Ticket',
+      data: ticket_data,
+      backgroundColor:
+      [
+        'rgba(0,0,255, 0.5)',
+        'rgba(0,0,255, 0.5)',
+
+      ]
+    },
+    {
+      label: 'Call Handled',
+      data: handled_data,
+      backgroundColor:
+      [
+        'rgba(0,128,0, 0.5)',
+        'rgba(0,128,0, 0.5)'
+      ]
+    }]
+  };
+
+  var call_data = [123,456,789];
+  var callChart = {
+    labels: year,
+    datasets:
+    [{
+      label: 'Handled',
       data: call_data,
       backgroundColor:
       [
@@ -13,41 +78,17 @@
         'rgba(0,0,255, 0.5)',
         'rgba(0,128,0, 0.5)'
       ]
-    }]
-  };
-
-  var year2 = ['2018','2019','2020'];
-  var question_data = [123,456,789];
-  var questionChart =
-  {
-    labels: year2,
-    datasets:
-    [{
-      data: question_data,
+    },{
+      label: 'Handled',
+      data: call_data,
       backgroundColor:
       [
         'rgba(0,0,128, 0.5)',
         'rgba(0,0,255, 0.5)',
         'rgba(0,128,0, 0.5)'
       ]
-    }]
-  };
-
-  var year3 = ['2018','2019','2020'];
-  var kanwil_data = [123,456,789];
-  var kanwilChart =
-  {
-    labels: year3,
-    datasets:
-    [{
-      data: kanwil_data,
-      backgroundColor:
-      [
-        'rgba(0,0,128, 0.5)',
-        'rgba(0,0,255, 0.5)',
-        'rgba(0,128,0, 0.5)'
-      ]
-    }]
+    },
+  ]
   };
 
   var year4 = ['2018','2019','2020'];
@@ -69,10 +110,10 @@
 
   window.onload = function ()
   {
-    var aca = document.getElementById("annualCalls").getContext("2d");
-    var callBar = new Chart(aca,{
-      type: 'bar',
-      data: callChart,
+    var aca = document.getElementById("detailAnnualy").getContext("2d");
+    var ticketBar = new Chart(aca,{
+      type: 'line',
+      data: detailChart,
       options: {
         scales:{
           yAxes:[{
@@ -94,15 +135,15 @@
         responsive:true,
         title:{
           display: true,
-          text: 'Calls'
+          text: 'Detail Tickets'
         }
       }
     });
 
-    var acb = document.getElementById("annualKanwil").getContext("2d");
-    var kanwilBar = new Chart(acb, {
+    var acc = document.getElementById("ticketAnnualy").getContext("2d");
+    var questionBar = new Chart(acc, {
         type: 'bar',
-        data: kanwilChart,
+        data: ticketChart,
         options: {
           scales: {
              yAxes: [{
@@ -124,39 +165,39 @@
             responsive: true,
             title: {
                 display: true,
-                text: 'Kanwil Calls Report'
+                text: 'Tickets Annualy'
             }
         }
     });
 
-    var acc = document.getElementById("annualQuestion").getContext("2d");
-    var questionBar = new Chart(acc, {
-        type: 'bar',
-        data: questionChart,
-        options: {
-          scales: {
-             yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-             }],
-             xAxes: [{
-                     ticks: {
-                      fontSize: 10
-                     }
-                    }]
-            },
-            elements: {
-                rectangle: {
-                    borderSkipped: 'bottom'
-                }
-            },
-            responsive: true,
-            title: {
-                display: true,
-                text: 'FAQ Report'
+    var acb = document.getElementById("call").getContext("2d");
+    var kanwilBar = new Chart(acb, {
+      type: 'bar',
+      data: callChart,
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
             }
+          }],
+          xAxes: [{
+            ticks: {
+              fontSize: 10
+            }
+          }]
+        },
+        elements: {
+          rectangle: {
+            borderSkipped: 'bottom'
+          }
+        },
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Kanwil tickets Report'
         }
+      }
     });
 
     var acd = document.getElementById("annualCabang").getContext("2d");
@@ -184,7 +225,7 @@
             responsive: true,
             title: {
                 display: true,
-                text: 'Cabang Calls Report'
+                text: 'Cabang tickets Report'
             }
         }
     });
